4d618641892e6d9cf7089ae4efb8e639738fd42d
maxbe <maxbe>
2024-10-08 08:50:37 +0300

    acme

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+from .models import Birthday
 
 
 def birthday(request):
-    context = {}
+    form = BirthdayForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday'])
+        context.update({'birthday_countdown': birthday_countdown})
+
     return render(request, 'birthday/birthday.html', context=context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)

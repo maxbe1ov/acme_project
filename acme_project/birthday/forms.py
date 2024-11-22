5ccbd0eedc05786634f0db6a506c7c990e596edd
Maxim Belov <maxbe@exante.eu>
2024-11-23 02:31:59 +0300

    acme_projectc

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Birthday
+from .validators import real_age
+from django.core.exceptions import ValidationError
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 class BirthdayForm(forms.ModelForm):
@@ -10,3 +14,21 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+        validators = (real_age,),
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
+from django.core.paginator import Paginator
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None,
+                        files=request.FILES or None, instance=instance)
     context = {
         'form': form
     }
@@ -19,8 +25,35 @@ def birthday(request):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 2)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context) 
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
